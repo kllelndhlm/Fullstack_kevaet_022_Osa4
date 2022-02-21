b5d993fe47d56caf5a42c1992f65fe24b8c0a6f3
Kalle Lindholm <kalle.lindholm@helsinki.fi>
2022-02-21 22:33:16 +0200

    Tehtävä 4.15

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const config = require('./utils/config')
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
-const mongoose = require('mongoose')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -22,6 +23,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
