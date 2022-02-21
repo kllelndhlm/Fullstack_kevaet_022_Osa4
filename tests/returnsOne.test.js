7f18a2da55f39b0648537f490db7cf9b1e29b585
Kalle Lindholm <kalle.lindholm@helsinki.fi>
2022-02-21 02:43:22 +0200

    Tehtävä 4.4

--- tests/returnsOne.test.js
+++ tests/returnsOne.test.js
@@ -1,22 +1,7 @@
 const listHelper = require('../utils/list_helper')
 
 test('dummy returns one', () => {
-  const blogs = [  {
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 1,
-    __v: 0
-  },
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 0,
-    __v: 0
-  }]
+  const blogs = []
 
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,8 +1,20 @@
 const dummy = (blogs) => {
   let testingTesting = blogs.length / blogs.length
-  return testingTesting
+  return blogs.length === 0
+  ? 1 
+  : testingTesting
   }
-  
+
+const totalLikes = (blogs) => {
+  let initialValue = 0
+  let sum = blogs.reduce(
+      (previousValue, currentValue) => previousValue + currentValue.likes
+      , initialValue
+  )
+  return sum
+  }
+
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
